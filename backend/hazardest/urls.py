e226722d15e2ea29224e1c634be09d619882e003
slaman <jordan.slaman@gmail.com>
2017-08-21 19:56:01 -0700

    frontend/root right align buttons, urls/view cleanup

--- backend/hazardest/urls.py
+++ backend/hazardest/urls.py
@@ -1,21 +1,16 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from django.conf import settings
-from django.conf.urls.static import static
-
 from rest_framework import routers
 
-# from views import index, play
 import viewsets as CoreViewSets
+
 
 router = routers.DefaultRouter()
 
 router.register(r'session', CoreViewSets.SessionViewSet)
 
-
 urlpatterns = [
-
     url(r'^', include(router.urls)),
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
